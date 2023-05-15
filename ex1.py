853e521760a23322a81800e502b56016ef9e4a69
Luis <luisguibarreto8@gmail.com>
2023-05-15 09:07:02 -0300

    Adicionando arquivos

---
+++ ex1.py
@@ -0,0 +1,26 @@
+a = float(input('Informe o valor de a: '))
+b = float(input('Informe o valor de b: '))
+c = float(input('Informe o valor de c: '))
+
+def bhaskara():
+    delta = b ** 2 - 4 * a * c
+    if delta < 0:
+        frase += 'Não há raízes'
+    elif delta == 0:
+        frase += 'Ambas as raízes são iguais a '
+    else:
+        x1 = (-b + delta ** 0.5) / (2 * a)
+        x2 = (-b - delta ** 0.5) / (2 * a)
+        frase += f'x1 = {x1}   x2 = {x2}'
+    xv = -b/(2 * a)
+    yv = -delta/(4 * a)
+    frase += f' e o vértice está em xv = {xv} e yv = {yv}'
+    return frase
+bhaskara()
+
+
+
+
+    
+
+
